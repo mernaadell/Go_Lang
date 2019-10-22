85b7dcc3d9110e8ac4b1303e91aa6fe58369e492
MernaAdel <31679900+mernaadell@users.noreply.github.com>
2019-10-22 23:18:24 +0200

    "problems in go lang"

--- Problems.go
+++ Problems.go
@@ -1,127 +1,11 @@
 package main
 
 import "fmt"
-
-func main() {
-  var x float64
-   x = 20.0
-  var rate int =3
-  fmt.Println("rate",rate)
-   y := 42 
-   fmt.Println(x)
-   fmt.Println(y)
-   fmt.Printf("x is of type %T\n", x)
-   fmt.Println("hi")
-   fmt.Printf("y is of type %T \n", y)
-   fmt.Println("__________")
- var a, b, c = 3, 4, "foo"  //da satr 3la b3do
-	
-   fmt.Println(a)
-   fmt.Println(b)
-   fmt.Println(c)
-   fmt.Printf("a is of type %T\n", a)
-   fmt.Printf("b is of type %T\n", b)
-   fmt.Printf("c is of type %T\n", c)
- var x1=10 
- var y1=20
-  var f=x1+y1
-  fmt.Println(f)
-  fmt.Println("________________")
- if (x1>3) {
-      fmt.Println("Yes")
-  }
-     num := 10
-    if num % 2 == 0 { //checks if number is even
-        fmt.Println("the number is even") 
-    }  else {
-        fmt.Println("the number is odd")
-    }
-  for i:=0;i<10;i++{
-      fmt.Println(i)
-  }
-  switch rate{
-      
-     case 0:
-     fmt.Println("good")
-     break
-     case 1:
-     fmt.Println("very good")
-     break
-       case 2:
-     fmt.Println("very good")
-     break
-       case 3:
-     fmt.Println("very good")
-      break
-      case 4:
-     fmt.Println("very good")
-     default:
-     fmt.Println("bye")
-  }
-  fmt.Println("______functions")
-  mass()
-    nam("merna")
-   fmt.Println( add(2,3))
-   fmt.Println("-----------arrays-----")
-   var g [10] int
-   g[0]=1
-   g[2]=2
- }
- func mass(){
-     
-     fmt.Println("fuck you")
- }
- func nam (name string){
-     
-     fmt.Println(name)
- }
-  func add (x int ,y int) int{
-     
-     return x+y
- }
-//output
-/*
-rate 3
-20
-42
-x is of type float64
-hi
-y is of type int 
-__________
-3
-4
-foo
-a is of type int
-b is of type int
-c is of type string
-30
-________________
-Yes
-the number is even
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-very good
-______functions
-fuck you
-merna
-5
------------arrays-----*/
-package main
-
-import "fmt"
 import "math"
 
 func main() {
     var x int =0
- fmt.Println("1 leap year\n 2 calculate the moment\n")
+ fmt.Println("1 leap year\n2 calculate sum_differencet\n3 sum_multiples\n4 binary_decimal\n5 plaindorm\n6 prime_factors\n7 calc_seconds\n8 binary_search")
  fmt.Println("Enter the code number ")
  fmt.Scanln(&x) 
  fmt.Println("your choice is ",x)
@@ -131,7 +15,7 @@ func main() {
         leap_year()
          break;
         case 2:
-        fmt.Println(  sum_difference())
+        fmt.Println( "square_of_sum - sum_of_squares", sum_difference())
      break;
         case 3:
         fmt.Println(sum_multiples())
@@ -140,12 +24,16 @@ func main() {
        fmt.Print(binary_decimal())
      break;
         case 5:
+        plaindorm()
      break;
         case 6:
+        prime_factors()
      break;
         case 7:
+        calc_seconds()
      break;
         case 8:
+         binary_search()
      break;
      
  }
@@ -229,24 +117,102 @@ func main() {
      
  }
  func plaindorm(){
-       var str ="mena"
+       var str string
   fmt.Println("Enter the string")
-  //fmt.Scanln(&str)
+  fmt.Scanln(&str)
  //var str string;
-  var i int
+// var i int
   var flag int;
-//  var len =len(str)
+ var len2 =len(str)
   	flag = 0;
-  	for i:= 0; i < 5; i++{
-		if(str[i] != str[5 - i - 1]){
+  	for i:= 0; i < len2; i++{
+		if(str[i] != str[len2 - i - 1]){
 			flag = 1;
 			break;	
 		} 
 	}
 	if(flag == 0){
-	fmt.Printf("\n %s is a Palindrome String", str);
+	fmt.Printf(" %s is a Palindrome String", str);
 	}else{
-		fmt.Printf("\n %s is Not a Palindrome String", str);
+		fmt.Printf("%s is Not a Palindrome String", str);
 	}
     
+ }
+ func prime_factors(){
+       var num, isPrime int
+
+    /* Input a number from user */
+    fmt.Println("Enter any number to print Prime factors: ");
+     fmt.Scanln(&num)
+
+   fmt.Println("All Prime Factors  \n", num)
+
+    /* Find all Prime factors */
+    for i:=2; i<=num; i++ {
+        /* Check 'i' for factor of num */
+        if(num%i==0){
+            /* Check 'i' for Prime */
+            isPrime = 1
+            for j:=2; j<=i/2; j++ {
+                if(i%j==0){
+                    isPrime = 0
+                    break
+                }
+            }
+
+            /* If 'i' is Prime number and factor of num */
+            if(isPrime==1){
+                fmt.Printf("%d, ", i)
+            }
+        }
+    }
+ }
+ func calc_seconds(){
+var time int
+ fmt.Scanln(&time)
+var day = time / (24 * 3600)
+time = time % (24 * 3600)
+var hour = time / 3600
+time %= 3600
+var minutes = time / 60
+time %= 60
+var seconds = time
+var years=day/365
+day=day-(years*365)
+fmt.Println("years:days:hours:min:sec-> ",years,day, hour, minutes, seconds)
+ }
+ func binary_search(){
+      var c, first, last, middle, n, search int
+      var array[100]int
+ 
+  fmt.Printf("Enter number of elements\n")
+ fmt.Scanln(&n)
+ 
+   fmt.Printf("Enter %d integers\n", n);
+ 
+   for c = 0; c < n; c++{
+      
+         fmt.Scanln(&array[c])
+   }
+ 
+   fmt.Printf("Enter value to find\n")
+    fmt.Scanln(&search)
+    first = 0
+   last = n - 1
+  middle = (first+last)/2
+ 
+  for first <= last {
+      if (array[middle] < search){
+         first = middle + 1
+          
+      } else if (array[middle] == search) {
+         fmt.Printf("%d found at location %d.\n", search, middle+1)
+         break
+      }else{
+         last = middle - 1
+      }
+      middle = (first + last)/2
+   }
+   if (first > last){
+      fmt.Printf("Not found! %d isn't present in the list.\n", search)}
  }
